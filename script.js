e55ee2974024d619dcf53573337d10ef0f157fb6
SHANTANU KUMAR <79790478+shantanu400@users.noreply.github.com>
2024-06-23 16:05:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,19 +2,24 @@
 let choosen=document.getElementById('btn').addEventListener('click', function(){
 	 let a=document.getElementById('text').value;
 	 let b=document.getElementById('delay').value;
+	let c=document.getElementById('output');
+	
 	let Promisehai=new Promise((resolve,reject)=>{
 		setTimeout(()=>resolve(a),b);
          
 	});
 	async function func(){
 		let result= await Promisehai;
-		let c=document.getElementById('output');
+		
 	    c.innerHTML+=`
 		<p>${result}</p>
 		
 		
 	`;
+      document.getElementById('text').value = ''
+	document.getElementById('delay').value = ''
 	}
 	func();
+	
 	
 })
